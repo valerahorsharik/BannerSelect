ab68471d9d3c8fe731a343fdc37cdfad68bcb26a
Valera Horsharik <valera.horsharik@gmail.com>
2016-08-23 23:22:39 +0300

    Delete useless fields

--- banner.sql
+++ banner.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.5.1
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Июн 09 2016 г., 14:41
--- Версия сервера: 5.6.17
--- Версия PHP: 5.5.12
+-- Время создания: Авг 23 2016 г., 19:57
+-- Версия сервера: 5.7.11
+-- Версия PHP: 7.0.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- База данных: `banners`
@@ -26,26 +26,42 @@ SET time_zone = "+00:00";
 -- Структура таблицы `banner`
 --
 
-CREATE TABLE IF NOT EXISTS `banner` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL,
   `text` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `priority` int(3) NOT NULL,
-  `show` int(3) NOT NULL,
-  `available` int(11) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+  `priority` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Дамп данных таблицы `banner`
 --
 
-INSERT INTO `banner` (`id`, `text`, `priority`, `show`, `available`) VALUES
-(1, 'Banner1', 1, 0, 1),
-(2, 'Banner2', 3, 0, 1),
-(3, 'Banner3', 5, 0, 1),
-(4, 'Banner4', 2, 0, 1),
-(5, 'Banner5', 2, 0, 1);
+INSERT INTO `banner` (`id`, `text`, `priority`) VALUES
+(1, 'banner1.jpg', 1),
+(2, 'banner2.jpg', 3),
+(3, 'banner3.jpg', 5),
+(4, 'banner4.jpg', 2),
+(5, 'banner5.jpg', 2);
 
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `banner`
+--
+ALTER TABLE `banner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
